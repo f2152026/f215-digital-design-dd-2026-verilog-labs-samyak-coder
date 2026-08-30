018f1e544c99080a5a96817819cad803852c24c4
samyak-coder <chinuchopra123@google.com>
2026-08-30 11:19:05 +0530

    debugging 0

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(3) (ps,  a,   b);
-  and #(3) (pc1, a,   b);
-  xor #(3) (sum, cin, ps);
-  and #(3) (pc2, cin, ps);
-  or #(3)  (cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or #(2)  (cout, pc1, pc2);
 
 endmodule
